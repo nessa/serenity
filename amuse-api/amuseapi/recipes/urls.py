c941c4932909041c0cc709b1a93fe3ed49196199
Noelia Sales Montes <noelia.salesmontes@gmail.com>
2015-01-11 11:46:13 +0100

    Adding hyperlinks to the API.

--- amuse-api/amuseapi/recipes/urls.py
+++ amuse-api/amuseapi/recipes/urls.py
@@ -2,7 +2,26 @@
 from django.conf.urls import patterns, url
 from recipes import views
 
+# API endpoints
 urlpatterns = [
+    url(r'^$', views.api_root),
+    url(r'^recipes/$',
+        views.RecipeList.as_view(),
+        name='recipe-list'),
+    url(r'^recipes/(?P<pk>[0-9]+)/$',
+        views.RecipeDetail.as_view(),
+        name='recipe-detail'),
+    url(r'^users/$',
+        views.UserList.as_view(),
+        name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name='user-detail')
+]
+
+
+urlpatterns = [
+    url(r'^$', views.api_root),
     url(r'^recipes/$', views.RecipeList.as_view()),
     url(r'^recipes/(?P<pk>[0-9]+)/$', views.RecipeDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
--- amuse-api/amuseapi/recipes/views.py
+++ amuse-api/amuseapi/recipes/views.py
@@ -6,7 +6,17 @@ from api.users.models import User
 from recipes.serializers import UserSerializer
 from rest_framework import permissions
 from recipes.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
+
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'recipes': reverse('recipe-list', request=request, format=format)
+    })
 
 class RecipeList(generics.ListCreateAPIView):
     queryset = Recipe.objects.all()
