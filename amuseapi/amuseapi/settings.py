3c74135fdbf057dff2570d7b979673101e2ba510
Noelia Sales <noelia.salesmontes@gmail.com>
2015-06-19 17:35:31 +0200

    Add STATIC_ROOT variable to settings file to prevent collectstatic failure.

--- amuseapi/amuseapi/settings.py
+++ amuseapi/amuseapi/settings.py
@@ -138,3 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' 'static/' subdirectories and in STATICFILES_DIRS.
+# Example: '/home/media/media.lawrence.com/static/'
+STATIC_ROOT = os.path.join(BASE_PATH, 'static')
