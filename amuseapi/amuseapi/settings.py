e49f6ad47d14e0ce9bbe6e60a7d44f03389d71f3
Noelia Sales <noelia.salesmontes@gmail.com>
2015-06-19 17:37:56 +0200

    Add STATIC_ROOT variable to settings file to prevent collectstatic failure.

--- amuseapi/amuseapi/settings.py
+++ amuseapi/amuseapi/settings.py
@@ -143,4 +143,4 @@ STATIC_URL = '/static/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' 'static/' subdirectories and in STATICFILES_DIRS.
 # Example: '/home/media/media.lawrence.com/static/'
-STATIC_ROOT = os.path.join(BASE_PATH, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
