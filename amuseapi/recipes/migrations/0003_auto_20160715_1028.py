adaee1df135b7a655d7d825e4723209c58b54292
Noelia Sales <noelia.salesmontes@gmail.com>
2016-07-15 12:47:23 +0200

    Add new migration to load initial data.

---
+++ amuseapi/recipes/migrations/0003_auto_20160715_1028.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.core.management import call_command
+from django.db import models, migrations
+
+fixture = 'initial_data'
+
+def load_fixture(apps, schema_editor):
+    call_command('loaddata', fixture, app_label='recipes')
+        
+class Migration(migrations.Migration):
+    
+    dependencies = [
+        ('recipes', '0002_ingredient_ingredientcategory_translatedingredient'),
+    ]
+
+    operations = [
+        migrations.RunPython(load_fixture),
+    ]
